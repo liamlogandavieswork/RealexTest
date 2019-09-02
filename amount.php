a2adcb64bc15081e72234bfe19acd0cb6667b4fd
Liam Logan-Davies <ldavies@codec.ie>
2019-09-02 13:51:04 +0100

    decode json

--- amount.php
+++ amount.php
@@ -29,7 +29,7 @@ $paymentDataArray = array(
 
 $Data = json_encode($paymentDataArray);
 $actionResponse = useWebApi("POST", $Data, "msevtmgt_GetPurchaseDetailsAction", $config, $configParams);
-$response = json_encode($actionResponse);
+$response = json_decode($actionResponse);
 error_log($response);
 return $response;
 
