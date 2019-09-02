add02506d89aba66fc7675855b1e5f5a2cd1444c
Liam Logan-Davies <ldavies@codec.ie>
2019-09-02 13:12:55 +0100

    logging imploded

--- amount.php
+++ amount.php
@@ -8,8 +8,8 @@ $config = include('config.php');
 // grab the response sent by the client-side library
 $request = file_get_contents('php://input');
 $decoded_request = json_decode($request, true);
-//$test = implode(" ",decoded_request)
-error_log($decoded_request);
+$test = implode(" ",$decoded_request)
+error_log($test);
 
 $AzureApplicationID = $decoded_request["AzureApplicationID"];
 $AzureApplicationSecret = $decoded_request["AzureApplicationSecret"];
