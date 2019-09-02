f8b5f6820fa08255be05a23056ff77382534394c
Liam Logan-Davies <ldavies@codec.ie>
2019-09-02 13:32:22 +0100

    don't encode

--- amount.php
+++ amount.php
@@ -30,7 +30,7 @@ $paymentDataArray = array(
 $Data = json_encode($paymentDataArray);
 $actionResponse = useWebApi("POST", $Data, "msevtmgt_GetPurchaseDetailsAction", $config, $configParams);
 //error_log($actionResponse);
-$response = json_encode($actionResponse["Result"]);
+$response = $actionResponse["Result"];
 return $response;
 
 function getAppAccessToken($config, $configParams)
