dc8cb9d44ee8008a1659a992b4810cc7b0fa548b
Liam Logan-Davies <ldavies@codec.ie>
2019-09-02 13:46:10 +0100

    log json

--- amount.php
+++ amount.php
@@ -29,8 +29,8 @@ $paymentDataArray = array(
 
 $Data = json_encode($paymentDataArray);
 $actionResponse = useWebApi("POST", $Data, "msevtmgt_GetPurchaseDetailsAction", $config, $configParams);
-//error_log($actionResponse);
-$response = $actionResponse["Result"];
+$response = json_encode($actionResponse);
+error_log($response);
 return $response;
 
 function getAppAccessToken($config, $configParams)
